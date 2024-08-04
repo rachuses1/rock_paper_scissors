bf69ce2b9446a84ff403321c9b7837a755b12365
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-04 16:20:04 +1000

    Rejig the computer selection function

--- javascript.js
+++ javascript.js
@@ -1,10 +1,9 @@
-console.log("Hello World");
-
 function getComputerChoice(max) {
 
     // generate a random number 0 1 and 2
     let computerChoice = Math.floor(Math.random() * max);
     console.log(computerChoice);
+
     // If the random number is 0, then return "Rock"
     if (computerChoice === 0) {
         return "rock";}
@@ -18,20 +17,67 @@ function getComputerChoice(max) {
     }
 };
 
-function getHumanChoice() {
-    let humanChoice = prompt("Heya what do you choose?","");
-    console.log(humanChoice);
-}
+//const computerSelection = getComputerChoice(); // Store the result of getComputerChoice
 
-let humanScore = 0;
-let computerScore = 0;
+// ------------------------------------------------------------------ //
 
-function playRound(humanChoice,computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
+// let humanChoice;
 
-}
+// // create prompt to get input // 
+// function getHumanChoice() {
+//     let humanChoice = prompt("Heya what do you choose?","");
+//     console.log(`HumanChoice is ${humanChoice}`);
+// }
 
-console.log(getHumanChoice());
-console.log(getComputerChoice(3));
+// function playRound(humanChoice,computerSelection) {
+//     humanChoice = humanSelection.toLowerCase();
+//     // humanChoice winning conditions
+//     // humanChoice = paper computerSelection = rock
+//     // humanChoice = scissors computerSelection = paper
+//     // humanChoice = rock computerSelection = scissors 
 
+//     switch(true) {
+//         case (humanChoice === "paper" && computerSelection === "rock"):
+//             console.log(`You win! ${humanSelection} beats ${computerSelection}`);
+//             break;
+//         case (humanChoice === "scissors" && computerSelection === "paper"):
+//             console.log(`You win! ${humanSelection} beats ${computerSelection}`);
+//             break;        
+//         case (humanChoice === "rock" && computerSelection === "scissors"):
+//             console.log(`You win! ${humanSelection} beats ${computerSelection}`);
+//             break;
+        
+//     // computer winning conditions
+//     // humanChoice = paper computerSelection = scissors
+//     // humanChoice = scissors computerSelection = rock
+//     // humanChoice = rock computerSelection = paper
+//         case (humanChoice === "paper" && computerSelection === "scissors"):
+//             console.log(`You lose! ${computerSelection} beats ${humanSelection}`);
+//             break;
+        
+//         case (humanChoice === "scissors" && computerSelection === "rock"):
+//             console.log(`You lose! ${computerSelection} beats ${humanSelection}`);
+//             break;
+
+//         case (humanChoice === "rock" && computerSelection === "paper"):
+//             console.log(`You lose! ${computerSelection} beats ${humanSelection}`);
+//             break;
+        
+//     // tie conditions
+//     // humanChoice = computerSelection
+//         case (humanChoice === computerSelection):
+//             console.log(`You tie!`);
+//             break;
+//     }
+// }
+
+// // ------------------------------------------------------------//
+// //Score Keeping //
+// let humanScore = 0;
+// let computerScore = 0;
+
+
+//------------------------------------------------------------//
+// Call the functions //
+console.log("Computer Choice is",getComputerChoice(3));
 
