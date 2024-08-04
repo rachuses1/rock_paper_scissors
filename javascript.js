121f0ed56c25fade9fdb18d554e9efe2b2adf2c0
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-04 15:26:49 +1000

    Add conditionals for the human winning conditions

--- javascript.js
+++ javascript.js
@@ -3,20 +3,35 @@ console.log("Hello World");
 function getComputerChoice(max) {
 
     // generate a random number 0 1 and 2
-    let choice = Math.floor(Math.random() * max);
-    console.log(choice);
+    let computerChoice = Math.floor(Math.random() * max);
+    console.log(computerChoice);
     // If the random number is 0, then return "Rock"
-    if (choice === 0) {
-        return "Rock";}
+    if (computerChoice === 0) {
+        return "rock";}
 
-    else if (choice === 1) {
-        return "Paper";
+    else if (computerChoice === 1) {
+        return "paper";
     } 
     
     else {
-        return "Scissors";
+        return "scissors";
     }
 };
 
+function getHumanChoice() {
+    let humanChoice = prompt("Heya what do you choose?","");
+    console.log(humanChoice);
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice,computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+}
+
+console.log(getHumanChoice());
 console.log(getComputerChoice(3));
+
 
